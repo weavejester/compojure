6e78ed9202a801510b30d36167d8f8d42d0316e6
Jim <jim@monkeyking.(none)>
2008-05-02 21:23:10 +0100

    Added number? function

--- base/general.clj
+++ base/general.clj
@@ -1,6 +1,12 @@
 (in-ns 'clojure)
 
 (defn include?
-  "Returns true if x is included in coll."
+  "Returns true if x is contained in coll, else false."
   [x coll]
     (some (partial = x) coll))
+
+(defn number?
+  "Returns true if an Clojure number type."
+  [x]
+  (or (instance? clojure.lang.FixNum x)
+      (instance? clojure.lang.DoubleNum x)))
