280e78255f925de0b631c01e110319dfb5ba76c8
Jim <jim@monkeyking.(none)>
2008-06-18 23:54:41 +0100

    Made Jetty server interface more idiomatic

--- modules/jetty/server.clj
+++ modules/jetty/server.clj
@@ -11,32 +11,44 @@
   (ref {}))
 
 (defn get-continuation
+  "Get the current Jetty continuation object for the request."
   [request]
   (. ContinuationSupport (getContinuation request request)))
 
 (defn suspend-request
+  "Use Jetty's pseudo-continuation support to suspend handling a HTTP request.
+  When called, the current servlet ends and is re-called when the request is
+  resumed. When resumed, suspend-request exits immediatedly.
+    Requests are stored in a hash with a user defined key, so you can resume
+  a group of requests at once (e.g. all the requests in one chat room)."
   ([request key]
     (suspend-request request key *default-timeout*))
   ([request key timeout]
     (let [cc (get-continuation request)]
       (dosync
-        (let [cset (*continuations* key)]
-          (alter *continuations*
-            #(assoc % key (set (conj cset cc))))))
+        (let [cset    (*continuations* key)
+              cset+cc (conj (set cset) cc)]
+          (alter *continuations* assoc key cset+cc)))
       (. cc (suspend timeout)))))
 
-(defn resume-request
+(defn resume-requests
+  "Resumes a group of suspended requests by key. See: suspend-request."
   [key]
-  (doseq continuation
-    (dosync (return (*continuations* key)
-              (alter *continuations* #(dissoc % key))))
-    (. continuation (resume))))
+  (let [cset (dosync (return (*continuations* key)
+                       (alter *continuations* dissoc key)))]
+    (doseq cc cset
+      (. cc (resume)))))
 
+(def resume-request resume-requests) ; An alias for resume-requests
+
+; Bind suspend-request to all resource calls, so you don't have to manually
+; specify the request object each time.
 (add-resource-binding
   'suspend-request
   `(partial suspend-request ~'request))
 
 (defn http-server
+  "Create a new Jetty HTTP server with the supplied servlet."
   [servlet & options]
   (let [options (apply hash-map options)
         port    (or (options :port) 8080)
