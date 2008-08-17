87ed3e0587a3b0a5b194fde34548f32b1ec9c7d1
Jim <jim@monkeyking.(none)>
2008-08-17 17:24:01 +0100

    Jetty interface now supports multiple servlets

--- config/boot.clj
+++ config/boot.clj
@@ -6,7 +6,9 @@
 
 ; Create a new Jetty HTTP Server on port 8080
 (def *server*
-  (http-server resource-servlet :port 8080))
+  (http-server
+    :port     8080
+    :servlets { "/*" resource-servlet }))
 
 (ANY "/*" (not-found))              ; Add in default 404 route
 (GET "/*" (serve-file (route :*)))  ; Add in a static file handler
--- lib/compojure/jetty/jetty.clj
+++ lib/compojure/jetty/jetty.clj
@@ -9,13 +9,13 @@
 
 (defn http-server
   "Create a new Jetty HTTP server with the supplied servlet."
-  [servlet & options]
+  [& options]
   (let [options (apply hash-map options)
         port    (or (options :port) 8080)
         server  (new Server port)
-        context (new Context server "/" (. Context SESSIONS))
-        holder  (new ServletHolder servlet)]
-    (. context (addServlet holder "/*"))
+        context (new Context server "/" (. Context SESSIONS))]
+    (doseq [path servlet] (options :servlets)
+      (.addServlet context (new ServletHolder servlet) path))
     server))
 
 (defn start "Start a HTTP server."
