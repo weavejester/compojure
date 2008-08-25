26b0a0ebd4de7cd8dc66cf90c91663bf57e2b7cd
Jim <jim@monkeyking.(none)>
2008-08-25 17:55:56 +0100

    First bits of cometd support

--- config/boot.clj
+++ config/boot.clj
@@ -2,13 +2,14 @@
 (refer 'clojure.contrib.lib)
 
 ; Load all the Compojure libraries
-(use '(compojure html http jetty json persist))
+(use '(compojure cometd html http jetty json persist))
 
 ; Create a new Jetty HTTP Server on port 8080
 (def *server*
   (http-server
     :port     8080
-    :servlets { "/*" resource-servlet }))
+    :servlets { "/cometd/*" cometd-servlet
+                "/*"        resource-servlet }))
 
 (ANY "/*" (not-found))              ; Add in default 404 route
 (GET "/*" (serve-file (route :*)))  ; Add in a static file handler
---
+++ lib/compojure/cometd/cometd.clj
@@ -0,0 +1,14 @@
+;; An interface to Jetty's cometd implementation
+(init-ns 'compojure.cometd)
+
+(import '(org.mortbay.cometd.continuation ContinuationCometdServlet))
+
+(def cometd-servlet
+  (new ContinuationCometdServlet))
+
+(defn- get-channel
+  "Gets a cometd channel that we can publish or subscribe to."
+  [channel]
+  (.. cometd-servlet
+      (getBayeux)
+      (getChannel channel true)))
