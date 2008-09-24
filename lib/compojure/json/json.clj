9c968fc88230ac6a94a919d5f99eca6289b37e4e
Jim <jim@monkeyking.(none)>
2008-09-24 21:16:23 +0100

    Tidied up JSON library

--- lib/compojure/json/json.clj
+++ lib/compojure/json/json.clj
@@ -1,17 +1,15 @@
-;; Turn Clojure datatypes into a JSON string
-(ns compojure.json)
+;; compojure.json
+;;
+;; Small library for formatting standard Clojure datatype as a JSON string.
 
-(use '(clojure.contrib str-utils))
+(ns compojure.json
+  (:use (clojure.contrib str-utils)))
 
 (def json)
 
-(defn json-list
-  [coll]
-  (str-join ", " (map json coll)))
-
 (defn- json-array
   [coll]
-  (str "[" (json-list coll) "]"))
+  (str "[" (str-join ", " (map json coll)) "]"))
 
 (defn- json-object
   [hash]
@@ -20,12 +18,14 @@
     (str "{" (str-join ", " (map pair hash)) "}")))
 
 (defn json
-  "Parse a Clojure datatype into a portable JSON format."
+  "Format a Clojure datatype into a portable JSON format."
   [x]
   (cond
-    (nil? x)             "null"
-    (vector? x)          (json-array x)
-    (map? x)             (json-object x)
-    (string? x)          (pr-str x)
-    (keyword? x)         (pr-str (name x))
-    (instance? Number x) (pr-str x)))
+    (nil? x)     "null"
+    (seq? x)     (json-array x)
+    (vector? x)  (json-array x)
+    (map? x)     (json-object x)
+    (string? x)  (pr-str x)
+    (keyword? x) (pr-str (name x))
+    (number? x)  (pr-str x)
+                 (.toString x)))
