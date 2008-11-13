d50cac2ec52c02fa680eadac191899842d7c0bb9
Jim <jim@monkeyking.(none)>
2008-11-13 00:19:57 +0000

    Changed randomly generated test values to manually chosen values

--- test/compojure/html/html.clj
+++ test/compojure/html/html.clj
@@ -2,93 +2,108 @@
   (:use fact
         (compojure html)))
 
-(defn rand-tags
-  "Generate a sequence of random tag names." []
-  (rand-strs (str ascii-letters "-") 1 5))
+(def names
+  '(foo Bar foo-bar foo_bar fooBar))
+
+(fact "The first element of a tag vector defines the tag name"
+  [tag names]
+  (= (xml [tag "Lorem Ipsum"])
+     (str "<" tag ">Lorem Ipsum</" tag ">")))
 
 (fact "Tag vectors can be empty"
-  [tag (rand-tags)]
+  [tag names]
   (= (xml [tag])
      (str "<" tag " />")))
 
-(fact "Tag vectors can contain strings"
-  [tag     (rand-tags)
-   content (rand-strs)]
-  (= (xml [tag content])
-     (str "<" tag ">" content "</" tag ">")))
-
 (fact "Tags can be specified by keywords, symbols or strings"
-  [tag [:hr 'hr "hr"]]
-  (= (xml [tag])
-     (str "<hr />")))
+  [tag [:xml 'xml "xml"]]
+  (= (xml [tag "Lorem Ipsum"])
+     "<xml>Lorem Ipsum</xml>"))
+
+(def test-contents
+  ["Lorem"
+   "Lorem Ipsum"
+   ""
+   "Lorem\nIpsum"
+   "  Lorem"
+   "Ipsum  "
+   "  Lorem\n    Ipsum"
+   "Lorem<br />Ipsum"])
+
+(fact "Tag vectors can contain strings"
+  [content test-contents]
+  (= (xml [:xml content])
+     (str "<xml>" content "</xml>")))
+
+(def test-seqs
+  ['("a" "b")
+   '("a" " b")
+   '("a" "\n" "b")
+   '("a " "b " "c " "d")])
 
 (fact "Tag vectors concatenate their contents"
-  [tag      (rand-tags)
-   contents (rand-seqs rand-strs 1 10)]
-  (= (xml (apply vector tag contents))
-     (str "<" tag ">" (apply str contents) "</" tag ">")))
+  [contents test-seqs]
+  (= (xml (apply vector :xml contents))
+     (str "<xml>" (apply str contents) "</xml>")))
 
 (fact "Sequences in tag vectors are expanded out"
-  [tag      (rand-tags)
-   contents (rand-seqs rand-strs 1 10)]
-  (= (xml (apply vector tag contents))
-     (xml [tag contents])))
+  [contents test-seqs]
+  (= (xml (apply vector :xml contents))
+     (xml [:xml contents])))
 
 (fact "Tag vectors can be nested"
-  [outer-tag  (rand-tags)
-   before     (rand-strs)
-   inner-tag  (rand-tags)
-   inner-str  (rand-strs)
-   after      (rand-strs)]
-  (= (xml [outer-tag
-            before [inner-tag inner-str] after])
-     (xml [outer-tag
-            before (xml [inner-tag inner-str]) after])))
-
-(defn rand-attr-pairs
-  "Generate a random attribute-value pair." []
-  (map vector
-    (rand-strs (str ascii-letters "-") 2 10)
-    (rand-strs (str ascii-letters) 0 30)))
+  [dom [[:a [:b]]
+        [:a "b" [:c] "d"]
+        [:a [:b "c"]]
+        [:a [:b [:c "d"]]]]
+   out ["<a><b /></a>"
+        "<a>b<c />d</a>"
+        "<a><b>c</b></a>"
+        "<a><b><c>d</c></b></a>"]]
+  (= (xml dom) out))
 
 (fact "Tag vectors can have attribute maps"
-  [tag        (rand-tags)
-   [attr val] (rand-attr-pairs)
-   content    (rand-strs)]
-  (= (xml [tag {attr val} content])
-     (str "<" tag " " attr "=\"" val "\">"
-          content
-          "</" tag ">")))
+  [attr-map [{:a "1"}
+             {:b "1" :a "2"}
+             {:b "2" :c "3" :a "1"}
+             {:aBc "d"}
+             {:a_b "c"}
+             {:a:b "c"}
+             {:something_rather_long "some text"}]
+   attr-str ["a=\"1\""
+             "a=\"2\" b=\"1\""
+             "a=\"1\" b=\"2\" c=\"3\""
+             "aBc=\"d\""
+             "a_b=\"c\""
+             "a:b=\"c\""
+             "something_rather_long=\"some text\""]]
+  (= (xml [:xml attr-map "Lorem Ipsum"])
+     (str "<xml " attr-str ">Lorem Ipsum</xml>")))
 
-(fact "Attributes maps can have many values"
-  [attr-map [{:a "1" :b "2" :c "3" :d "4"}
-             {:id "a" :class "b"}]
-   attr-str ["a=\"1\" b=\"2\" c=\"3\" d=\"4\""
-             "class=\"b\" id=\"a\""]]
-  (= (xml [:div attr-map])
-     (str "<div " attr-str " />")))
-
-(fact "Special characters are escaped in attributes"
-  [char    ["\""     "<"    ">"    "&"]
+(fact "Special characters are escaped in attribute values"
+  [char    ["\"" "<" ">" "&"]
    escaped ["&quot;" "&lt;" "&gt;" "&amp;"]]
   (= (xml [:div {:id char}])
      (str "<div id=\"" escaped "\" />")))
 
+(def inline-tags
+  '(a span em strong code img))
+
 (fact "HTML tag vectors have syntax sugar for class attributes"
-  [tag   (rand-tags)
-   class (rand-strs ascii-letters 1 20)]
+  [tag   inline-tags
+   class names]
   (= (html [tag {:class class}])
      (html [(str tag "." class)])))
 
 (fact "HTML tag vectors have syntax sugar for id attributes"
-  [tag (rand-tags)
-   id  (rand-strs ascii-letters 1 20)]
+  [tag inline-tags
+   id  names]
   (= (html [tag {:id id}])
      (html [(str tag "#" id)])))
 
 (fact "The contents of HTML block tags are indented")
 
 (fact "The HTML pre tag is rendered without indentation"
-  [content (rand-strs)]
+  [content test-contents]
   (= (html [:body [:div [:pre content]]])
      "<body>\n  <div>\n    <pre>" content "</pre>\n  </div>\n</body>\n"))
--- test/test.clj
+++ test/test.clj
@@ -1,5 +1,5 @@
 (use 'fact)
 (use 'test.compojure.html)
 
-(.println *test-out* "Facts:")
+(.println *test-out* "compojure.html:")
 (print-results (verify-facts 'test.compojure.html))
