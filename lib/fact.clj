6af5c8bc604c07b615037c2575efff54a0a72eb4
Jim <jim@monkeyking.(none)>
2008-11-27 22:09:14 +0000

    Updated Fact to version with stubbing

--- lib/fact.clj
+++ lib/fact.clj
@@ -71,8 +71,24 @@
           :pending? ~(nil? value-map)
           :params  '~(vec params)
           :values   ~(vec values))))))
-      
-(def #^{:doc "The maximum amount of random test values to use."}
+
+(defmacro stub
+  "Create function stubs for isolated unit tests.
+  e.g. (stub [(f 1 2) 3
+              (f 3 2) 5]
+         (= (+ (f 1 2) (f 3 2))
+            8))"
+  [stubs & body]
+  (let [stub-pairs (partition 2 stubs)
+        fold-map   (fn [acc [[f & args] ret]]
+                      (assoc-in acc [f `(list ~@args)] ret))
+        bind-stub  (fn [[fname fhash]]
+                     `(~fname (fn [& args#] (~fhash args#))))]
+    `(binding
+       [~@(mapcat bind-stub (reduce fold-map {} stub-pairs))]
+       ~@body)))
+
+(def #^{:doc "The maximum amount of test values to use per fact."}
   *max-amount* 50)
 
 (defn- test-cases
