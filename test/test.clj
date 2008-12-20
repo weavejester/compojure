a6d12ee5c531202596c0c02efa1c9a37826e0bb6
weavejester <jreeves@weavejester.com>
2008-12-20 20:11:58 +0000

    Updated Fact to display unit test results in colour

--- test/test.clj
+++ test/test.clj
@@ -4,12 +4,12 @@
                           validation))
 
 (.println *test-out* "compojure.html:")
-(print-results (verify-facts 'test.compojure.html))
+(print-color-results (verify-facts 'test.compojure.html))
 (.println *test-out*)
 
 (.println *test-out* "compojure.http:")
-(print-results (verify-facts 'test.compojure.http))
+(print-color-results (verify-facts 'test.compojure.http))
 (.println *test-out*)
 
 (.println *test-out* "compojure.validation:")
-(print-results (verify-facts 'test.compojure.validation))
+(print-color-results (verify-facts 'test.compojure.validation))
