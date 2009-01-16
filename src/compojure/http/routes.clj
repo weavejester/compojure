74765573fb308d494823eafe36439372a5790b09
weavejester <jreeves@weavejester.com>
2009-01-16 01:11:44 +0000

    Removed extra space indent from routes file header

--- src/compojure/http/routes.clj
+++ src/compojure/http/routes.clj
@@ -1,15 +1,15 @@
-;;  Copyright (c) James Reeves. All rights reserved.
-;;  The use and distribution terms for this software are covered by the Eclipse
-;;  Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which
-;;  can be found in the file epl-v10.html at the root of this distribution. By
-;;  using this software in any fashion, you are agreeing to be bound by the
-;;  terms of this license. You must not remove this notice, or any other, from
-;;  this software.
+;; Copyright (c) James Reeves. All rights reserved.
+;; The use and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which
+;; can be found in the file epl-v10.html at the root of this distribution. By
+;; using this software in any fashion, you are agreeing to be bound by the
+;; terms of this license. You must not remove this notice, or any other, from
+;; this software.
 
-;;  compojure.http.routes:
+;; compojure.http.routes:
 ;; 
-;;  Functions compiling and matching routes contructed using the Rails routing
-;;  syntax.
+;; Functions compiling and matching routes contructed using the Rails routing
+;; syntax.
 
 (ns compojure.http.routes
   (:use compojure.str-utils))
