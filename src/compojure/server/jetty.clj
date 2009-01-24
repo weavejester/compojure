68537ca8fd95c99a7572cb4fe25f596b2e963ff3
weavejester <jreeves@weavejester.com>
2009-01-24 19:03:18 +0000

    Added virtual host support to compojure.server.jetty

--- src/compojure/server/jetty.clj
+++ src/compojure/server/jetty.clj
@@ -11,12 +11,12 @@
 ;; Clojure interface to start an embedded Jetty server.
 
 (ns compojure.server.jetty
-  (:import org.mortbay.jetty Server)
+  (:use compojure.control)
+  (:import java.net.URL)
+  (:import org.mortbay.jetty.Server)
   (:import org.mortbay.jetty.servlet.Context)
   (:import org.mortbay.jetty.servlet.ServletHolder)
   (:import org.mortbay.util.ajax.ContinuationSupport))
-
-;;;;; HTTP server ;;;;
 
 (defn servlet-holder
   "Wrap a servlet in a ServletHolder object with a supplied set of parameters
@@ -28,19 +28,47 @@
       (.setInitParameter holder (name key) (str val)))
     holder))
 
+(defn get-context
+  "Get a Context instant for a server and hostname."
+  ([server]
+    (get-context server nil))
+  ([server host]
+    (let [context (Context. server "/" Context/SESSIONS)]
+      (if host
+        (doto context (.setVirtualHosts (into-array [host])))
+        context))))
+
+(decorate-with memoize get-context)
+
+(defn- get-host-and-path
+  "Splits a path or URL into its hostname and path."
+  [url-or-path]
+  (if (re-matches #"^/w+://" url-or-path)
+    (let [url (.URL url-or-path)]
+      [(.getHost url) (.getPath url)])
+    [nil url-or-path]))
+
+(defn add-servlet!
+  "Add a servlet to a Jetty server. Servlets can be connected to a relative
+  path or an absolute URL. When connected to a URL, the function will try and
+  use the hostname to set up a virtual host. Wildcards for the domain and path
+  are allowed."
+  [server url-or-path servlet]
+  (let [[host path] (get-host-and-path url-or-path)
+        context     (get-context server host)
+        holder      (if (instance? ServletHolder servlet)
+                       servlet
+                      (ServletHolder. servlet))]
+    (.addServlet context holder path)))
+
 (defn- create-server
   "Construct a Jetty Server instance."
   [options servlets]
   (let [port     (or (options :port) 8080)
-        server   (new Server port)
-        context  (new Context server "/" (. Context SESSIONS))
+        server   (Server. port)
         servlets (partition 2 servlets)]
-    (doseq [[path servlet] servlets]
-      (.addServlet context
-                   (if (instance? ServletHolder servlet)
-                     servlet
-                     (new ServletHolder servlet))
-                   path))
+    (doseq [[url-or-path servlet] servlets]
+      (add-servlet! server url-or-path servlet))
     server))
 
 (defn http-server
