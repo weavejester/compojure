faa45ef0764812b2c2b11de14d033fa3d3da9d00
weavejester <jreeves@weavejester.com>
2009-01-24 15:50:50 +0000

    Added copyright notice to compojure.jetty

--- src/compojure/jetty.clj
+++ src/compojure/jetty.clj
@@ -1,8 +1,20 @@
-;; Jetty interface for Compojure
+;; Copyright (c) James Reeves. All rights reserved.
+;; The use and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which
+;; can be found in the file epl-v10.html at the root of this distribution. By
+;; using this software in any fashion, you are agreeing to be bound by the
+;; terms of this license. You must not remove this notice, or any other, from
+;; this software.
+
+;; compojure.jetty:
+;; 
+;; Clojure interface to start an embedded Jetty server.
+
 (ns compojure.jetty
-  (:import (org.mortbay.jetty Server)
-           (org.mortbay.jetty.servlet Context ServletHolder)
-           (org.mortbay.util.ajax ContinuationSupport)))
+  (:import org.mortbay.jetty Server)
+  (:import org.mortbay.jetty.servlet.Context)
+  (:import org.mortbay.jetty.servlet.ServletHolder)
+  (:import org.mortbay.util.ajax.ContinuationSupport))
 
 ;;;;; HTTP server ;;;;
 
