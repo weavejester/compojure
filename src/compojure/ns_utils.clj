d85ce15d89b4ef8f26301197cadbc66d7a0b2af4
weavejester <jreeves@weavejester.com>
2009-01-27 01:53:37 +0000

    Updated immigrate function using code suggested by Stephen C. Gilardi

--- src/compojure/ns_utils.clj
+++ src/compojure/ns_utils.clj
@@ -1,19 +1,15 @@
 (ns compojure.ns-utils)
 
-(defn- merge-meta!
-  "Destructively merge metadata from a source object into a target."
-  [source target]
-  (.setMeta target
-    (merge (meta source)
-           (select-keys (meta target) [:name :ns]))))
-
 (defn immigrate
-  "Add all the public vars in a list of namespaces to the current namespace."
-  [& namespaces]
-  (doseq [ns namespaces]
-    (require ns)
-    (doseq [[sym v] (ns-publics (find-ns ns))]
-      (merge-meta! v
-        (if (.isBound v)
-          (intern *ns* sym (var-get v))
-          (intern *ns* sym))))))
+ "Create a public var in this namespace for each public var in the
+ namespaces named by ns-names. The created vars have the same name, value,
+ and metadata as the original except that their :ns metadata value is this
+ namespace."
+ [& ns-names]
+ (doseq [ns ns-names]
+   (require ns)
+   (doseq [[sym var] (ns-publics ns)]
+     (let [sym (with-meta sym (assoc (meta var) :ns *ns*))]
+       (if (.isBound var)
+         (intern *ns* sym (var-get var))
+         (intern *ns* sym))))))
