94a847e81f8be3183e5d3a1fd2795b5cbcf2bd53
weavejester <jreeves@weavejester.com>
2009-01-27 01:03:56 +0000

    Tidied up compojure.control, removed ifn and otherwise

--- src/compojure/control.clj
+++ src/compojure/control.clj
@@ -1,14 +1,17 @@
-;; Macros and functions for controling program flow
+;; Copyright (c) James Reeves. All rights reserved.
+;; The use and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which
+;; can be found in the file epl-v10.html at the root of this distribution. By
+;; using this software in any fashion, you are agreeing to be bound by the
+;; terms of this license. You must not remove this notice, or any other, from
+;; this software.
+
+;; compojure.control:
+;; 
+;; Various macros for controling program flow.
+
 (ns compojure.control
-  (:use (clojure.contrib seq-utils)))
-
-(def #^{:doc "Synonym for true; useful for ending conds"}
-  otherwise true)
-
-(defn ifn
-  "Equivalent to: (if (pred x) x (func x))"
-  [pred func x]
-  (if (pred x) x (func x)))
+  (:use clojure.contrib.seq-utils))
 
 (defmacro return
   "A do block that will always return the argument 'x'."
@@ -19,7 +22,7 @@
 (defmacro domap
   "Similar to doseq, but collects the results into a sequence."
   [[item list] & body]
-  `(map (fn [~item] ~@body) ~list))
+  `(map (fn [~item] ~@body) (doall ~list)))
 
 (defmacro redef
   "Redefine an existing value, keeping the metadata intact."
