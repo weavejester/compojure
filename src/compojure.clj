32b2bd5eb4650b942374da62a38660858d6eef8c
weavejester <jreeves@weavejester.com>
2009-01-27 01:07:09 +0000

    Added docs and header to compojure package library

--- src/compojure.clj
+++ src/compojure.clj
@@ -1,7 +1,20 @@
+;; Copyright (c) James Reeves. All rights reserved.
+;; The use and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which
+;; can be found in the file epl-v10.html at the root of this distribution. By
+;; using this software in any fashion, you are agreeing to be bound by the
+;; terms of this license. You must not remove this notice, or any other, from
+;; this software.
+
+;; compojure:
+;; 
+;; Convenience library that includes every compojure.* namespace. If you want
+;; to access Compojure quickly, and don't care about having everything in one
+;; namespace, just use or require 'compojure.
+
 (ns compojure
   (:use compojure.ns-utils))
 
-;; Import all Compojure namespaces
 (immigrate
   'compojure.control
   'compojure.file-utils
