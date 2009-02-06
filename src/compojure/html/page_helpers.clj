159f621dab9d3a6ba36e6c3a709b6f09239e2c3e
weavejester <jreeves@weavejester.com>
2009-02-06 21:18:37 +0000

    Applied doctype patch from Abhishek Reddy

--- src/compojure/html/page_helpers.clj
+++ src/compojure/html/page_helpers.clj
@@ -19,15 +19,15 @@
 
 (def doctype
   {:html4
-   (str "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\""
+   (str "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\" "
         "\"http://www.w3.org/TR/html4/strict.dtd\">\n")
 
    :xhtml-strict
-   (str "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\""
+   (str "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" "
         "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n")
 
    :xhtml-transitional
-   (str "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\""
+   (str "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" "
         "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n")})
 
 (defn xhtml-tag
