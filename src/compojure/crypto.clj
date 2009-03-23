ee80c5d8fb1497c74e6464d9c07ab093f7e4c905
weavejester <jreeves@weavejester.com>
2009-03-23 21:10:30 +0000

    Added compojure.crypto (again!)

---
+++ src/compojure/crypto.clj
@@ -0,0 +1,26 @@
+;; Copyright (c) James Reeves. All rights reserved.
+;; The use and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which
+;; can be found in the file epl-v10.html at the root of this distribution. By
+;; using this software in any fashion, you are agreeing to be bound by the
+;; terms of this license. You must not remove this notice, or any other, from
+;; this software.
+
+;; compojure.crypto:
+;;
+;; Functions for cryptographically signing, verifying and encrypting data.
+
+(ns compojure.crypto
+  (:use compojure.encodings)
+  (:import javax.crypto.Mac)
+  (:import javax.crypto.spec.SecretKeySpec))
+
+(defn hmac
+  "Generate a hashed message authentication code with the supplied key and
+  algorithm on some string data."
+  [key algorithm data]
+  (let [spec  (SecretKeySpec. (.getBytes key) algorithm)
+        mac   (doto (Mac/getInstance algorithm)
+                (.init spec))
+        bytes (.doFinal mac (.getBytes data))]
+    (base64-encode-bytes bytes)))
