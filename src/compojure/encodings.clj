7ed72b8c0d0874f753d1db4bbba73efd20e56081
weavejester <jreeves@weavejester.com>
2009-03-23 20:43:34 +0000

    Changed crypto to encodings, added Base64 support from Apache Commons

--- src/compojure/encodings.clj
+++ src/compojure/encodings.clj
@@ -6,15 +6,16 @@
 ;; terms of this license. You must not remove this notice, or any other, from
 ;; this software.
 
-;; compojure.crypto:
+;; compojure.encodings:
 ;;
-;; Functions for encoding, hashing and encrypting data.
+;; Functions for encoding data.
 
-(ns compojure.crypto
+(ns compojure.encodings
   (:use compojure.str-utils)
   (:use clojure.contrib.duck-streams)
   (:import java.net.URLEncoder)
-  (:import java.net.URLDecoder))
+  (:import java.net.URLDecoder)
+  (:import org.apache.commons.codec.binary.Base64))
 
 (defn urlencode
   "Encode a urlencoded string using the default encoding."
@@ -25,3 +26,28 @@
   "Decode a urlencoded string using the default encoding."
   [s]
   (URLDecoder/decode s *default-encoding*))
+
+(defn base64-encode-bytes
+  "Encode an array of bytes into a base64 encoded string."
+  [unencoded]
+  (String. (Base64/encodeBase64 unencoded)))
+ 
+(defn base64-encode
+  [unencoded]
+  "Encode a string using base64."
+  (base64-encode-bytes (.getBytes unencoded)))
+ 
+(defn base64-decode
+  "Decode a string using base64."
+  [encoded]
+  (String. (Base64/decodeBase64 (.getBytes encoded))))
+
+(defn marshal
+  "Serialize a Clojure object in a base64-encoded string."
+  [data]
+  (base64-encode (pr-str data)))
+ 
+(defn unmarshal
+  "Unserialize a Clojure object from a base64-encoded string."
+  [marshaled]
+  (read-string (base64-decode marshaled)))
