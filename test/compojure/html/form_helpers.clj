f05c54d70d88a76a37aa44a4e56bcb8574a9ca5f
weavejester <jreeves@weavejester.com>
2009-04-04 01:54:01 +0100

    Converted form-helpers to test-is

--- test/compojure/html/form_helpers.clj
+++ test/compojure/html/form_helpers.clj
@@ -1,10 +1,26 @@
 (ns test.compojure.html.form-helpers
-  (:use fact.core)
-  (:use compojure.html.form-helpers))
+  (:use compojure.html.form-helpers)
+  (:use clojure.contrib.test-is))
 
-(fact "Options in select lists can have different text and values"
-  []
-  (= (select-options [["a" "1"] ["b" "2"] ["c" "3"]])
-    '([:option {:value "1"} "a"]
-      [:option {:value "2"} "b"]
-      [:option {:value "3"} "c"])))
+(defn attribute
+  "Get an attribute from a tag vector."
+  [tag key]
+  ((second tag) key))
+
+(deftest test-text-field
+  (is (= (text-field :foo)
+         [:input {:type "text", :id "foo", :name "foo"}])))
+
+(deftest form-input-attrs
+  (let [field (text-field {:style "color: red;"} :foo)]
+    (is (= (attribute field :style) "color: red;"))))
+
+(deftest test-with-params
+  (is (= (with-params {:foo "bar"} (text-field :foo))
+         [:input {:type "text", :id "foo", :name "foo", :value "bar"}])))
+         
+(deftest select-options-with-values
+  (is (= (select-options [["a" "1"] ["b" "2"] ["c" "3"]])
+        '([:option {:value "1"} "a"]
+          [:option {:value "2"} "b"]
+          [:option {:value "3"} "c"]))))
--- test/run.clj
+++ test/run.clj
@@ -1,6 +1,8 @@
 (ns test.run
   (:use clojure.contrib.test-is)
-  (:require test.compojure.html.gen))
+  (:require test.compojure.html.gen)
+  (:require test.compojure.html.form-helpers))
 
 (run-tests 
-  'test.compojure.html.gen)
+  'test.compojure.html.gen
+  'test.compojure.html.form-helpers)
