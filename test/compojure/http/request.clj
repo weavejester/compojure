fa50e70aeead83d22e615de364936f736ba73360
weavejester <jreeves@weavejester.com>
2009-04-04 23:07:05 +0100

    Converted test.compojure.http.request to test-is

--- test/compojure/http/request.clj
+++ test/compojure/http/request.clj
@@ -1,73 +1,53 @@
 (ns test.compojure.http.request
-  (:use fact.core)
-  (:use fact.random-utils)
-  (:use re-rand)
-  (:use clojure.contrib.str-utils)
   (:use compojure.http.request)
+  (:use clojure.contrib.test-is)
   (:import java.io.ByteArrayInputStream))
 
-(defn- random-params []
-  (random-map random-keyword #(re-rand #"\w+") 1 10))
+(deftest query-params
+  (are (= (get-query-params {:query-string _1}) _2)
+    "a=1"     {:a "1"}
+    "a=1&b=2" {:a "1", :b "2"}))
 
-(defn- naively-encode
-  "Encode a map of parameters without urlencoding them first."
-  [params sep]
-  (str-join sep (for [[k v] params] (str (name k) "=" v))))
+(deftest query-params-plus
+  (is (= (get-query-params {:query-string "a=1+2"})
+         {:a "1 2"})))
 
-(defn- input-stream
-  "Create an input stream from a string"
-  [s]
+(deftest query-params-space
+  (is (= (get-query-params {:query-string "a=1%202"})
+         {:a "1 2"})))
+
+(deftest query-params-empty
+  (is (= (get-query-params {}) {})))
+
+(deftest query-params-merge
+  (let [request {:query-params {:a "1"}, :query-string "b=2"}]
+    (is (= (get-query-params request)
+           {:a "1", :b "2"}))))
+
+(deftest urlencoded-charset
+  (is (urlencoded-form?
+        {:content-type "application/x-www-form-urlencoded; charset=UTF8"})))
+
+(defn- input-stream [s]
   (ByteArrayInputStream. (.getBytes s)))
 
-(fact "Parameters can be passed via the query string"
-  [params random-params]
-  (let [query   (naively-encode params "&")
-        request {:query-string query}]
-    (= (get-query-params request)
-       params)))
+(defn- form-request [body]
+  {:content-type "application/x-www-form-urlencoded"
+   :body (input-stream body)})
 
-(fact "The form-urlencoded content-type can have an charset"
-  [charset #"\w+"]
-  (let [type (str "application/x-www-form-urlencoded; charset=" charset)]
-    (urlencoded-form? {:content-type type})))
+(deftest form-params
+  (are (= (get-form-params (form-request _1)) _2)
+    "a=1"     {:a "1"}
+    "a=1&b=2" {:a "1", :b "2"}))
 
-(fact "Parameters can be passed via the body"
-  [params random-params]
-  (let [body    (input-stream (naively-encode params "&"))
-        type    "application/x-www-form-urlencoded"
-        request {:content-type type, :body body}]
-    (= (get-form-params request)
-       params)))
+(deftest form-params-empty
+  (is (= (get-form-params {}) {})))
 
-(fact "Cookies can be passed via the 'cookie' HTTP header"
-  [cookies random-params]
-  (let [headers {"cookie" (naively-encode cookies "; ")}
-        request {:headers headers}]
-    (= (get-cookies request)
-       cookies)))
+(deftest form-params-merge
+  (let [request (merge {:form-params {:a "1"}} (form-request "b=2"))]
+    (is (= (get-form-params request)
+           {:a "1", :b "2"}))))
 
-(fact "get-query-params returns empty map if no parameters found"
-  []
-  (= (get-query-params {}) {}))
-
-(fact "get-form-params returns empty map if no parameters found"
-  [request [{} {:body ""}]]
-  (= (get-query-params {}) {}))
-
-(fact "get-query-params preserves parameters from request"
-  [params1 random-params
-   params2 random-params]
-  (let [query   (naively-encode params2 "&")
-        request {:query-params params1, :query-string query}]
-    (= (get-query-params request)
-       (merge params1 params2))))
-
-(fact "get-form-params preserves parameters from request"
-  [params1 random-params
-   params2 random-params]
-  (let [body    (naively-encode params2 "&")
-        request {:form-params  params1
-                 :content-type "application/x-www-form-urlencoded"
-                 :body         (input-stream body)}]
-    (= (get-form-params request)
-       (merge params1 params2))))
+(deftest request-cookies
+  (is (= (get-cookies {:headers {"cookie" "a=1;b=2"}})
+         {:a "1", :b "2"})))
--- test/run.clj
+++ test/run.clj
@@ -2,9 +2,11 @@
   (:use clojure.contrib.test-is)
   (:require test.compojure.html.gen)
   (:require test.compojure.html.form-helpers)
-  (:require test.compojure.http.routes))
+  (:require test.compojure.http.routes)
+  (:require test.compojure.http.request))
 
 (run-tests 
   'test.compojure.html.gen
   'test.compojure.html.form-helpers
-  'test.compojure.http.routes)
+  'test.compojure.http.routes
+  'test.compojure.http.request)
