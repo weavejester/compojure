3cde34266dec0443c0001637d792f242ad5ab33d
weavejester <jreeves@weavejester.com>
2009-04-07 22:27:27 +0100

    Slimmed down comments in compojure.http.grizzly

--- src/compojure/server/grizzly.clj
+++ src/compojure/server/grizzly.clj
@@ -8,37 +8,8 @@
 
 ;; compojure.server.grizzly
 ;; 
-;; Clojure interface to start an embedded Grizzly server.
-;;
-;; For now, grizzly support is not compiled or included by default
-;; when you require or use compojure.  To try grizzly:
-;;
-;; 1. Download the latest grizzly-servlet-webserver binary
-;;    from the http://grizzly.dev.java.net.  As of now:
-;;    http://download.java.net/maven/2/com/sun/grizzly/grizzly-servlet-webserver/1.9.10/grizzly-servlet-webserver-1.9.10.jar
-;; 2. Place the above file in deps.
-;; 3. Run ant:
-;;    ant -Dwith.grizzly
-;; 4. Put the new compojure.jar in your classpath.
-;;    
-;; You must require it with an alias or it will conflict with jetty.
-;; Use it the same way you used jetty.
-;;
-;; (require ['compojure.server.grizzly :as 'grizzly])
-;; (grizzly/run-server {:port 8081} "/*" (servlet my-app))
-;; 
-;; Jetty doesn't set any headers by default, so browsers assume
-;; text/html.  Grizzly defaults to text/plain so your route will need
-;; {:headers {"Content-Type" "text/html"}}.
-;;
-;; (defroutes my-app
-;;   (GET "/grizzly"
-;;     [{:headers {"Content-Type" "text/html"}}
-;;     "<html><body><h1>Hello Grizzly.</h1></body></html>"])
-;;   (GET "/*"
-;;     (or (serve-file (params :*)) :next))
-;;   (ANY "*"
-;;     (page-not-found)))
+;; Clojure interface to start an embedded Grizzly server. To compile, use:
+;;   ant -Dwith.grizzly
 
 (ns compojure.server.grizzly
   (:use compojure.control)
