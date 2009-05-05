6baa93c3df2020ba1f0cae7156a1bb51cf5abe56
Nicolas Buduroi <nbuduroi@gmail.com>
2009-05-06 07:03:26 +0800

    Removed blank? from validation namespace as there's already one in str-utils.
    Signed-off-by: James Reeves <jreeves@weavejester.com>

--- src/compojure/validation/predicates.clj
+++ src/compojure/validation/predicates.clj
@@ -1,10 +1,5 @@
 (ns compojure.validation)
 
-(defn blank?
-  "True if x is nil or an empty string."
-  [x]
-  (or (nil? x) (= x "")))
-
 (defn present?
   "True if x is not nil and not an empty string."
   [x]
