7361f9cc4240f5f90ab065755da14b90060e63af
Nicolas Buduroi <nbuduroi@gmail.com>
2009-05-06 06:54:47 +0800

    Added some tests for form helpers.
    Signed-off-by: James Reeves <jreeves@weavejester.com>

--- test/compojure/html/form_helpers.clj
+++ test/compojure/html/form_helpers.clj
@@ -11,6 +11,10 @@
   (is (= (text-field :foo)
          [:input {:type "text", :id "foo", :name "foo"}])))
 
+(deftest test-password-field
+  (is (= (password-field "foo")
+         [:input {:type "password", :id "foo", :name "foo" :value ""}])))
+
 (deftest form-input-attrs
   (let [field (text-field {:style "color: red;"} :foo)]
     (is (= (attribute field :style) "color: red;"))))
@@ -18,12 +22,27 @@
 (deftest test-with-params
   (is (= (with-params {:foo "bar"} (text-field :foo))
          [:input {:type "text", :id "foo", :name "foo", :value "bar"}])))
-         
+
+(deftest test-check-box
+  (is (= (check-box :foo)
+         [:input {:type "checkbox"
+                  :id "foo"
+                  :name "foo"
+                  :value "true"
+                  :checked nil}])))
+
 (deftest select-options-with-values
   (is (= (select-options [["a" "1"] ["b" "2"] ["c" "3"]])
         '([:option {:value "1"} "a"]
           [:option {:value "2"} "b"]
           [:option {:value "3"} "c"]))))
+
+(deftest drop-down-selected
+  (is (= (drop-down :foo [["a" "1"] ["b" "2"] ["c" "3"]] "2")
+         [:select {:id "foo" :name "foo"}
+          '([:option {:value "1"} "a"]
+            [:option {:value "2" :selected "selected"} "b"]
+            [:option {:value "3"} "c"])])))
 
 (deftest test-form-to
   (let [form (form-to [:post "action"] [])]
