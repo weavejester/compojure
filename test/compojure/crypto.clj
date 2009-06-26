5cf8f819f826c925fb0e730bfae5f334e2d548d9
weavejester <jreeves@weavejester.com>
2009-06-26 20:36:42 +0100

    Added some generic crypto tests back in

--- test/compojure/crypto.clj
+++ test/compojure/crypto.clj
@@ -2,9 +2,34 @@
   (:use compojure.crypto)
   (:use clojure.contrib.test-is))
 
+(deftest secret-key-length
+  (are (= (count (gen-secret-key {:key-size _1})) _2)
+    256 32
+    128 16))
+
+(deftest secret-key-uniqueness
+  (let [a (gen-secret-key {:key-size 128})
+        b (gen-secret-key {:key-size 128})]
+    (is (not= a b))))
+
 (def secret-key
   (.getBytes "0123456789ABCDEF"))
 
-(deftest test-seal-unseal
-  (is (= (unseal secret-key (seal secret-key "Foobar"))
-         "Foobar")))
+(deftest seal-string
+  (is (not= (seal secret-key "Foobar") "Foobar")))
+
+(deftest seal-uniqueness
+  (let [a (seal secret-key "Foobar")
+        b (seal secret-key "Foobar")]
+    (is (not= a b))))
+
+(deftest seal-then-unseal
+  (are (= (unseal secret-key (seal secret-key _1)) _1)
+    "Foobar"
+    [1 2 3]
+    {:a 10}))
+
+(deftest seal-then-tamper
+  (let [data (seal secret-key "Foobar")
+        data (apply str "A" (rest data))]
+    (is (nil? (unseal secret-key "Foobar")))))
