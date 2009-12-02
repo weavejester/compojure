3ed0a8f7c55c3678fa1ef55d3110b572f934df2e
Tom Crayford <tcrayford@googlemail.com>
2009-12-03 01:21:16 +0800

    added html5 to the list of doctypes

--- src/compojure/html/page_helpers.clj
+++ src/compojure/html/page_helpers.clj
@@ -19,6 +19,9 @@
    (str "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\" "
         "\"http://www.w3.org/TR/html4/strict.dtd\">\n")
 
+	 :html5
+	  (str "<!DOCTYPE html>")
+
    :xhtml-strict
    (str "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" "
         "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n")
--- test/compojure/html/page_helpers.clj
+++ test/compojure/html/page_helpers.clj
@@ -12,6 +12,10 @@
          (str "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" "
               "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n"))))
 
+(deftest test-doctype-html5
+	(is (= (doctype :html5)
+			  (str "<!DOCTYPE html>"))))
+
 (deftest test-doctype-xhtml-transitional
   (is (= (doctype :xhtml-transitional)
          (str  "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" "
