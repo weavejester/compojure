cb36033d672f16de59b4956734317c42eff582ca
Aaron Bedra <aaron@aaronbedra.com>
2010-01-08 10:22:53 -0500

    extending page_helpers_test

--- test/compojure/html/page_helpers_test.clj
+++ test/compojure/html/page_helpers_test.clj
@@ -2,36 +2,46 @@
   (:use compojure.html.page-helpers
         clojure.contrib.test-is))
 
-(deftest test-doctype-html4
-  (is (= (doctype :html4)
-         (str "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\" "
-              "\"http://www.w3.org/TR/html4/strict.dtd\">\n"))))
-
-(deftest test-doctype-xhtml-strict
-  (is (= (doctype :xhtml-strict)
-         (str "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" "
+(deftest test-doctype
+  (testing "html4"
+    (is (= (doctype :html4)
+           (str "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\" "
+                "\"http://www.w3.org/TR/html4/strict.dtd\">\n"))))
+  (testing "xhtml-strict"
+    (is (= (doctype :xhtml-strict)
+           (str "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" "
               "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n"))))
-
-(deftest test-doctype-html5
-	(is (= (doctype :html5)
-			  (str "<!DOCTYPE html>"))))
-
-(deftest test-doctype-xhtml-transitional
-  (is (= (doctype :xhtml-transitional)
-         (str  "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" "
-               "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n"))))
+  (testing "html5"
+    (is (= (doctype :html5)
+           (str "<!DOCTYPE html>"))))
+  (testing "xhtml-transitional"
+    (is (= (doctype :xhtml-transitional)
+           (str  "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" "
+                 "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n")))))
 
 (deftest test-xhtml-tag
   (is (= (xhtml-tag "test")
          [:html {:xmlns "http://www.w3.org/1999/xhtml", "xml:lang" "test", :lang "test"} nil])))
 
 (deftest test-include-js
-  (is (= (include-js "foo.js")
-         '([:script {:type "text/javascript", :src "foo.js"}]))))
+  (testing "one"
+    (is (= (include-js "foo.js")
+           '([:script {:type "text/javascript", :src "foo.js"}]))))
+  (testing "many"
+    (is (= (include-js "foo.js" "bar.js" "baz.js")
+           '([:script {:type "text/javascript", :src "foo.js"}]
+             [:script {:type "text/javascript", :src "bar.js"}]
+             [:script {:type "text/javascript", :src "baz.js"}])))))
 
 (deftest test-include-css
-  (is (= (include-css "foo.css")
-         '([:link {:type "text/css" :href "foo.css" :rel "stylesheet"}]))))
+  (testing "one"
+    (is (= (include-css "foo.css")
+           '([:link {:type "text/css" :href "foo.css" :rel "stylesheet"}]))))
+  (testing "many"
+    (is (= (include-css "foo.css" "bar.css" "baz.css")
+           '([:link {:type "text/css", :href "foo.css", :rel "stylesheet"}]
+             [:link {:type "text/css", :href "bar.css", :rel "stylesheet"}]
+             [:link {:type "text/css", :href "baz.css", :rel "stylesheet"}])))))
 
 (deftest test-javascript-tag
   (is (= (javascript-tag "alert('hi');")
