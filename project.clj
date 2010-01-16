0b537f5aac31579d9d9fcf9ace4f771802a55d9a
weavejester <jreeves@weavejester.com>
2010-01-16 17:42:33 +0000

    Replaced Ant build file with Leiningen project file

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject compojure "0.4.0-SNAPSHOT"
+  :description "A concise web framework for Clojure"
+  :url "http://github/weavejester/compojure/tree/refactor"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [clout "0.1"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
