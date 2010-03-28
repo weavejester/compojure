05fdace600424f67895b9596374ff343751762bd
weavejester <jreeves@weavejester.com>
2010-03-28 00:14:13 +0000

    Updated project URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject compojure "0.4.0-SNAPSHOT"
   :description "A concise web framework for Clojure"
-  :url "http://github/weavejester/compojure/tree/refactor"
+  :url "http://github/weavejester/compojure"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [clout "0.2.0-SNAPSHOT"]
