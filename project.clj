0f3b6b5a84bec84a77be5ef44c4290f5808d12c8
weavejester <jreeves@weavejester.com>
2010-04-21 02:05:07 +0100

    Removed SNAPSHOT from lein-clojars dependency

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
                  [org.clojure/clojure-contrib "1.1.0"]
                  [clout "0.2.0-SNAPSHOT"]
                  [ring/ring-core "0.2.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+  :dev-dependencies [[lein-clojars "0.5.0"]
                      [ring/ring-jetty-adapter "0.2.0"]
                      [hiccup "0.2.1"]])
