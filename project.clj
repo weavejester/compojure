e7c7b49df42c7867c2f31ef4a5df496ca5eaa347
weavejester <jreeves@weavejester.com>
2010-05-10 00:59:39 +0100

    Released version 0.4.0-RC1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject compojure "0.4.0-SNAPSHOT"
+(defproject compojure "0.4.0-RC1"
   :description "A concise web framework for Clojure"
   :url "http://github/weavejester/compojure"
   :dependencies [[org.clojure/clojure "1.1.0"]
@@ -7,5 +7,4 @@
                  [ring/ring-core "0.2.1"]]
   :dev-dependencies [[lein-clojars "0.5.0"]
                      [leiningen/lein-swank "1.1.0"]
-                     [ring/ring-jetty-adapter "0.2.0"]
-                     [hiccup "0.2.4"]])
+                     [ring/ring-jetty-adapter "0.2.0"]])
