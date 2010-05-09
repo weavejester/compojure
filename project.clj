d1918e946975ee0438f30bc83304cd21a5b8a73b
weavejester <jreeves@weavejester.com>
2010-05-09 16:37:18 +0100

    Updated dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://github/weavejester/compojure"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [clout "0.2.0-SNAPSHOT"]
-                 [ring/ring-core "0.2.0"]]
+                 [clout "0.2.0"]
+                 [ring/ring-core "0.2.1"]]
   :dev-dependencies [[lein-clojars "0.5.0"]
                      [leiningen/lein-swank "1.1.0"]
                      [ring/ring-jetty-adapter "0.2.0"]
-                     [hiccup "0.2.1"]])
+                     [hiccup "0.2.4"]])
