c122f5e86cb0fd5ca5751ea474f8f78890460e97
weavejester <jreeves@weavejester.com>
2010-05-17 21:35:21 +0100

    Released version 0.4.0-RC3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject compojure "0.4.0-RC2"
+(defproject compojure "0.4.0-RC3"
   :description "A concise web framework for Clojure"
   :url "http://github/weavejester/compojure"
   :dependencies [[org.clojure/clojure "1.1.0"]
