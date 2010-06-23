4251b02f494805b0dcbe36cfaf898ddef44a83d4
weavejester <jreeves@weavejester.com>
2010-06-23 20:35:19 +0100

    Updated ring dependency, released 0.4.0 final

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject compojure "0.4.0-RC3"
+(defproject compojure "0.4.0"
   :description "A concise web framework for Clojure"
   :url "http://github/weavejester/compojure"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [clout "0.2.0"]
-                 [ring/ring-core "0.2.2"]]
+                 [ring/ring-core "0.2.3"]]
   :dev-dependencies [[lein-clojars "0.5.0"]
                      [swank-clojure "1.2.0"]
                      [ring/ring-jetty-adapter "0.2.0"]])
