f3e7ea5d348bfc2007c69d4559324adb81b5189f
weavejester <jreeves@weavejester.com>
2010-07-13 21:37:13 +0100

    Updated project dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [clout "0.2.0"]
-                 [ring/ring-core "0.2.3"]]
+                 [ring/ring-core "0.2.5"]]
   :dev-dependencies [[lein-clojars "0.5.0"]
-                     [swank-clojure "1.2.0"]
-                     [ring/ring-jetty-adapter "0.2.0"]])
+                     [swank-clojure "1.2.1"]
+                     [ring/ring-jetty-adapter "0.2.5"]])
