5ba538359475b1f433d4bbf06fce9c46929ff86b
weavejester <jreeves@weavejester.com>
2010-09-10 17:31:34 +0100

    Updated dependencies to Clojure 1.2.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject compojure "0.4.1"
+(defproject compojure "0.5.0-beta1"
   :description "A concise web framework for Clojure"
   :url "http://github/weavejester/compojure"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
-                 [clout "0.2.0"]
-                 [ring/ring-core "0.2.5"]]
-  :dev-dependencies [[lein-clojars "0.5.0"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [clout "0.3.0"]
+                 [ring/ring-core "0.3.0-RC1"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]
                      [swank-clojure "1.2.1"]
-                     [ring/ring-jetty-adapter "0.2.5"]])
+                     [ring/ring-jetty-adapter "0.3.0-RC1"]])
