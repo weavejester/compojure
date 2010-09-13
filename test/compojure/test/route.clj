bcafbeddaffe227bea1812354426eea088b77648
weavejester <jreeves@weavejester.com>
2010-09-13 23:40:10 +0100

    Added route/resources function and unit tests

---
+++ test/compojure/test/route.clj
@@ -0,0 +1,15 @@
+(ns compojure.test.route
+  (:use clojure.test
+        [clojure.contrib.duck-streams :only (slurp*)])
+  (:require [compojure.route :as route]))
+
+(deftest not-found-route
+  (let [response ((route/not-found "foo") {})]
+    (is (= (:status response) 404))
+    (is (= (:body response) "foo"))))
+
+(deftest resources-route
+  (let [route    (route/resources "/foo" {:root "resources"})
+        response (route {:request-method :get, :uri "/foo/test.txt"})]
+    (is (= (:status response) 200))
+    (is (= (slurp* (:body response)) "foobar\n"))))
