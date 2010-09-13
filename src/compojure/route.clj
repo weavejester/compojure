5e3543f78ccdd17c09504c872b4f603b5946363a
weavejester <jreeves@weavejester.com>
2010-09-13 23:40:10 +0100

    Added route/resources function and unit tests

--- src/compojure/route.clj
+++ src/compojure/route.clj
@@ -1,6 +1,6 @@
 (ns compojure.route
   (:use compojure.core
-        [ring.util.response :only (file-response)]
+        [ring.util.response :only (file-response resource-response)]
         [ring.util.codec :only (url-decode)]))
 
 (defn- add-wildcard
@@ -9,13 +9,25 @@
   (str path (if (.endsWith path "/") "*" "/*")))
 
 (defn files
-  "A route for serving static files from a directory."
+  "A route for serving static files from a directory. Accepts the following
+  keys:
+    :root - the root path where the files are stored. Defaults to 'public'."
   [path & [options]]
   (GET (add-wildcard path) {{file-path "*"} :params}
     (let [options (merge {:root "public"} options)]
       (file-response file-path options))))
 
+(defn resources
+  "A route for serving resources on the classpath. Accepts the following
+  keys:
+    :root - the root prefix to get the resources from. Defaults to 'public'."
+  [path & [options]]
+  (GET (add-wildcard path) {{resource-path "*"} :params}
+    (let [options (merge {:root "public"} options)]
+      (resource-response resource-path options))))
+
 (defn not-found
-  "A route that returns a 404 not found response."
+  "A route that returns a 404 not found response, with its argument as the
+  response body."
   [body]
   (ANY "*" [] {:status 404, :body body}))
