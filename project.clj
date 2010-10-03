a421849c31328ed45f4b6180bc4bae5e8af76b92
weavejester <jreeves@weavejester.com>
2010-10-03 13:21:10 +0100

    Updated Ring and Clout dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://github/weavejester/compojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [clout "0.3.0"]
-                 [ring/ring-core "0.3.0"]]
+                 [clout "0.3.1"]
+                 [ring/ring-core "0.3.1"]]
   :dev-dependencies [[lein-clojars "0.6.0"]
                      [swank-clojure "1.2.1"]
-                     [ring/ring-jetty-adapter "0.3.0"]])
+                     [ring/ring-jetty-adapter "0.3.1"]])
