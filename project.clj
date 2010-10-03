dd3ad3aa373acb5f79e622382393a485927995c4
weavejester <jreeves@weavejester.com>
2010-10-03 13:21:44 +0100

    Released 0.5.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject compojure "0.5.1"
+(defproject compojure "0.5.2"
   :description "A concise web framework for Clojure"
   :url "http://github/weavejester/compojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
