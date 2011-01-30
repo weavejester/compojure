3f6ffe5a1b0908e900c1b476dad482227772d14b
James Reeves <jreeves@weavejester.com>
2011-01-30 20:03:32 +0000

    Released 0.6.0-RC3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject compojure "0.6.0-RC2"
+(defproject compojure "0.6.0-RC3"
   :description "A concise web framework for Clojure"
   :url "http://github/weavejester/compojure"
   :dependencies
