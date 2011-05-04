ff0c1c77e0841ce0b94b98f4be413eddd76cba88
James Reeves <jreeves@weavejester.com>
2011-05-04 22:31:02 +0100

    Updated Ring dependency to 0.3.8

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/tools.macro "0.1.0"]
                  [clout "0.4.1"]
-                 [ring/ring-core "0.3.7"]]
+                 [ring/ring-core "0.3.8"]]
   :dev-dependencies [[ring-mock "0.1.1"]
                      [org.clojure/clojure-contrib "1.2.0"]])
