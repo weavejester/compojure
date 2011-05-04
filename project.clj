0c1a87bd6545178c58cc254be9fa498e86274286
James Reeves <jreeves@weavejester.com>
2011-05-04 22:30:38 +0100

    Removing autodoc as a dependency until it works with Leiningen 1.5

--- project.clj
+++ project.clj
@@ -7,17 +7,4 @@
                  [clout "0.4.1"]
                  [ring/ring-core "0.3.7"]]
   :dev-dependencies [[ring-mock "0.1.1"]
-                     [org.clojure/clojure-contrib "1.2.0"]
-                     [autodoc "0.7.1" :exclusions [org.clojure/clojure
-                                                   org.clojure/clojure-contrib]]]
-  :autodoc
-    {:name "Compojure"
-     :description "A concise web framework for Clojure"
-     :copyright "Copyright 2011 James Reeves"
-     :root "."
-     :source-path "src"
-     :web-src-dir "http://github.com/weavejester/compojure/blob"
-     :web-home "http://weavejester.github.com/compojure"
-     :output-path "autodoc"
-     :namespaces-to-document ["compojure"]
-     :load-except-list [#"/test/" #"project.clj"]})
+                     [org.clojure/clojure-contrib "1.2.0"]])
