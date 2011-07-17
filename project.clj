512b03e006071e871a622d6e3503194458a5bb80
James Reeves <jreeves@weavejester.com>
2011-07-17 19:16:36 +0100

    Updated Ring and Compojure versions

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject compojure "0.6.4"
   :description "A concise web framework for Clojure"
   :url "https://github.com/weavejester/compojure"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/tools.macro "0.1.0"]
                  [clout "0.4.1"]
-                 [ring/ring-core "0.3.10"]]
+                 [ring/ring-core "0.3.11"]]
   :dev-dependencies [[ring-mock "0.1.1"]
                      [org.clojure/clojure-contrib "1.2.0"]])
