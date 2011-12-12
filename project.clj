e0bf0c0f7c69d8efdd7b2c59c504774732b49c9b
James Reeves <jreeves@weavejester.com>
2011-12-12 19:48:07 +0000

    Released 1.0.0-RC1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject compojure "1.0.0-SNAPSHOT"
+(defproject compojure "1.0.0-RC1"
   :description "A concise web framework for Clojure"
   :url "https://github.com/weavejester/compojure"
   :dependencies [[org.clojure/clojure "1.2.1"]
