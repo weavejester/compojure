68614932e60ebbf399b32c8393a399d26c3dcc7f
James Reeves <jreeves@weavejester.com>
2012-01-08 22:44:26 +0000

    Updated Clout to 1.0.1 (fixes #47)

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/tools.macro "0.1.0"]
-                 [clout "1.0.0"]
+                 [clout "1.0.1"]
                  [ring/ring-core "1.0.1"]]
   :dev-dependencies [[ring-mock "0.1.1"]
                      [org.clojure/clojure-contrib "1.2.0"]
