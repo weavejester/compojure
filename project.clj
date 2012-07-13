898da59b5fd0d14d73f8007bced32350f1086419
James Reeves <jreeves@weavejester.com>
2012-07-13 17:41:11 +0100

    Updated Ring dependency to 1.1.1

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/tools.macro "0.1.0"]
                  [clout "1.0.1"]
-                 [ring/ring-core "1.1.0"]]
+                 [ring/ring-core "1.1.1"]]
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.1"]]}})
