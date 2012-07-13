014cbf5f003791921a076feaceaea5efadc3bb09
James Reeves <jreeves@weavejester.com>
2012-07-13 17:42:37 +0100

    Added EPL license to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject compojure "1.1.0"
   :description "A concise web framework for Clojure"
   :url "https://github.com/weavejester/compojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/tools.macro "0.1.0"]
