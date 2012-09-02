2cef3f74bede4a8d7f2178b9fd2b999ddd311e34
James Reeves <jreeves@weavejester.com>
2012-09-02 22:25:07 +0100

    Updated dependencies

--- project.clj
+++ project.clj
@@ -7,6 +7,6 @@
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/tools.macro "0.1.0"]
                  [clout "1.0.1"]
-                 [ring/ring-core "1.1.1"]]
+                 [ring/ring-core "1.1.4"]]
   :profiles
-  {:dev {:dependencies [[ring-mock "0.1.1"]]}})
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
