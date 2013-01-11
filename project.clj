41ebb157f3233e55d372bcd6fcce631f43f3e14e
James Reeves <jreeves@weavejester.com>
2013-01-11 16:49:52 +0000

    Set version to 1.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject compojure "1.1.3"
+(defproject compojure "1.2.0-SNAPSHOT"
   :description "A concise routing library for Ring"
   :url "https://github.com/weavejester/compojure"
   :license {:name "Eclipse Public License"
