e0215de6e93670b8637b98a691e163ef7fa09f11
James Reeves <jreeves@weavejester.com>
2013-01-13 14:11:03 +0000

    Updated ring-core dependency to 1.1.7

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/tools.macro "0.1.0"]
                  [clout "1.1.0"]
-                 [ring/ring-core "1.1.5"]]
+                 [ring/ring-core "1.1.7"]]
   :plugins [[codox "0.6.4"]]
   :codox {:src-dir-uri "http://github.com/weavejester/compojure/blob/1.1.3"
           :src-linenum-anchor-prefix "L"}
