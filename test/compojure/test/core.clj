9f7c836c81e1ebfa0c50030f5b34a07a6c704aed
rod <rod@pu-gh.com>
2013-05-13 22:07:02 +0100

    made _method parameter case-insensitive

--- test/compojure/test/core.clj
+++ test/compojure/test/core.clj
@@ -57,6 +57,13 @@
           route (PUT "/foo" [] resp)]
       (is (= (route req) resp))))
 
+  (testing "_method parameter case-insenstive"
+    (let [req (-> (request :post "/foo")
+                  (assoc :form-params {"_method" "delete"}))
+          resp {:status 200, :headers {}, :body "bar"}
+          route (DELETE "/foo" [] resp)]
+      (is (= (route req) resp))))
+
   (testing "HEAD requests"
     (let [resp  {:status 200, :headers {"X-Foo" "foo"}, :body "bar"}
           route (GET "/foo" []  resp)]
