e8b2abf56c82835d1e4efd838a979bc0037a415a
James Reeves <jreeves@weavejester.com>
2013-10-30 01:52:50 +0000

    Updated Ring dependency to 1.2.1

--- project.clj
+++ project.clj
@@ -3,16 +3,15 @@
   :url "https://github.com/weavejester/compojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.incubator "0.1.0"]
                  [org.clojure/tools.macro "0.1.0"]
                  [clout "1.1.0"]
-                 [ring/ring-core "1.1.7"]]
+                 [ring/ring-core "1.2.1"]]
   :plugins [[codox "0.6.6"]]
   :codox {:src-dir-uri "http://github.com/weavejester/compojure/blob/1.1.3"
           :src-linenum-anchor-prefix "L"}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.3"]]}
-   :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
    :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
    :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
