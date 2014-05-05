932344ae6595b19670094e2fcf5f18313caeb8ae
James Reeves <jreeves@weavejester.com>
2014-05-05 14:39:12 +0100

    Fixed source links doc config

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [clout "1.1.0"]
                  [ring/ring-core "1.2.1"]]
   :plugins [[codox "0.6.6"]]
-  :codox {:src-dir-uri "http://github.com/weavejester/compojure/blob/1.1.6"
+  :codox {:src-dir-uri "http://github.com/weavejester/compojure/blob/1.1.6/"
           :src-linenum-anchor-prefix "L"}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.3"]]}
