3ad5c62b8d0103c9980526dde9cec90c8168a595
James Reeves <jreeves@weavejester.com>
2014-05-08 16:02:35 +0100

    Updated Codox to 0.7.4

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [org.clojure/tools.macro "0.1.0"]
                  [clout "1.1.0"]
                  [ring/ring-core "1.2.2"]]
-  :plugins [[codox "0.7.3"]]
+  :plugins [[codox "0.7.4"]]
   :codox {:src-dir-uri "http://github.com/weavejester/compojure/blob/1.1.7/"
           :src-linenum-anchor-prefix "L"}
   :profiles
