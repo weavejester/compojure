54219597480809317e44c3743b705a266becbfe7
James Reeves <jreeves@weavejester.com>
2014-11-29 20:04:14 +0000

    Update Ring dependency to 1.3.2

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/tools.macro "0.1.5"]
                  [clout "2.0.0"]
                  [medley "0.5.3"]
-                 [ring/ring-core "1.3.1"]
+                 [ring/ring-core "1.3.2"]
                  [ring/ring-codec "1.0.0"]]
   :plugins [[codox "0.8.10"]]
   :codox {:src-dir-uri "http://github.com/weavejester/compojure/blob/1.2.1/"
