f8c4208408d14a12bb2be9196d753cb8c272a6fc
James Reeves <jreeves@weavejester.com>
2016-03-07 02:27:29 +0000

    Update Medley to 0.7.3

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.macro "0.1.5"]
                  [clout "2.1.2"]
-                 [medley "0.7.1"]
+                 [medley "0.7.3"]
                  [ring/ring-core "1.4.0"]
                  [ring/ring-codec "1.0.0"]]
   :plugins [[lein-codox "0.9.4"]]
