a179bae528a33f8c89f9c9b7d37074a6b1dcceea
James Reeves <jreeves@weavejester.com>
2017-01-10 23:05:09 +0000

    Update Ring version to 1.6.0-beta7
    Fixes path traversal vulnerability in route/resources.

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/tools.macro "0.1.5"]
                  [clout "2.1.2"]
                  [medley "0.8.4"]
-                 [ring/ring-core "1.6.0-beta6"]
+                 [ring/ring-core "1.6.0-beta7"]
                  [ring/ring-codec "1.0.1"]]
   :plugins [[lein-codox "0.9.5"]]
   :codox
