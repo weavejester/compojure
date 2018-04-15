100a7887645c8ee087f159bb8aa98073257ab341
James Reeves <jreeves@weavejester.com>
2018-04-15 02:33:50 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,20 +5,21 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.macro "0.1.5"]
-                 [clout "2.1.2"]
+                 [clout "2.2.1"]
                  [medley "1.0.0"]
                  [ring/ring-core "1.6.3"]
-                 [ring/ring-codec "1.0.1"]]
+                 [ring/ring-codec "1.1.0"]]
   :plugins [[lein-codox "0.10.3"]]
   :codox
   {:output-path "codox"
    :metadata {:doc/format :markdown}
    :source-uri "http://github.com/weavejester/compojure/blob/{version}/{filepath}#L{line}"}
   :aliases
-  {"test-all" ["with-profile" "default:+1.8" "test"]}
+  {"test-all" ["with-profile" "default:+1.8:+1.9" "test"]}
   :profiles
   {:dev {:jvm-opts ^:replace []
-         :dependencies [[ring/ring-mock "0.3.0"]
+         :dependencies [[ring/ring-mock "0.3.2"]
                         [criterium "0.4.4"]
                         [javax.servlet/servlet-api "2.5"]]}
-   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}})
+   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+   :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}})
