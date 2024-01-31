2d794d7d3f7718711b33bb8dc4bacff0c073080d
James Reeves <jreeves@weavejester.com>
2024-01-31 18:51:43 +0000

    Update minimum Clojure version to 1.9.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/weavejester/compojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.macro "0.1.5"]
                  [clout "2.2.1"]
                  [medley "1.4.0"]
@@ -15,13 +15,11 @@
    :metadata {:doc/format :markdown}
    :source-uri "http://github.com/weavejester/compojure/blob/{version}/{filepath}#L{line}"}
   :aliases
-  {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10:+1.11" "test"]}
+  {"test-all" ["with-profile" "default:+1.10:+1.11" "test"]}
   :profiles
   {:dev {:jvm-opts ^:replace []
          :dependencies [[ring/ring-mock "0.4.0"]
                         [criterium "0.4.6"]
                         [javax.servlet/servlet-api "2.5"]]}
-   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
    :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}
    :1.11 {:dependencies [[org.clojure/clojure "1.11.1"]]}})
