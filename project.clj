e7500d105ebc073fbd1dec3dd1b6e0a595c40f04
Christian Johansen <christian@cjohansen.no>
2025-09-15 12:13:39 +0200

    Upgrade medley

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.macro "0.1.5"]
                  [clout "2.2.1"]
-                 [medley "1.4.0"]
+                 [dev.weavejester/medley "1.9.0"]
                  [ring/ring-core "1.11.0"]
                  [ring/ring-codec "1.2.0"]]
   :plugins [[lein-codox "0.10.7"]]
