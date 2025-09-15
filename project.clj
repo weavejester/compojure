0636ce6fa68d62c0bad88f138e072f8d408cebc8
James Reeves <jreeves@weavejester.com>
2025-09-15 15:36:36 +0100

    Update dependencies
    Closes #222.

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.macro "0.1.5"]
+                 [org.clojure/tools.macro "0.2.1"]
                  [clout "2.2.1"]
-                 [medley "1.4.0"]
-                 [ring/ring-core "1.11.0"]
-                 [ring/ring-codec "1.2.0"]]
-  :plugins [[lein-codox "0.10.7"]]
+                 [dev.weavejester/medley "1.9.0"]
+                 [ring/ring-core "1.15.1"]
+                 [ring/ring-codec "1.3.0"]]
+  :plugins [[lein-codox "0.10.8"]]
   :codox
   {:output-path "codox"
    :metadata {:doc/format :markdown}
@@ -18,8 +18,8 @@
   {"test-all" ["with-profile" "default:+1.10:+1.11" "test"]}
   :profiles
   {:dev {:jvm-opts ^:replace []
-         :dependencies [[ring/ring-mock "0.4.0"]
+         :dependencies [[ring/ring-mock "0.6.2"]
                         [criterium "0.4.6"]
                         [javax.servlet/servlet-api "2.5"]]}
    :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}
-   :1.11 {:dependencies [[org.clojure/clojure "1.11.1"]]}})
+   :1.11 {:dependencies [[org.clojure/clojure "1.11.4"]]}})
